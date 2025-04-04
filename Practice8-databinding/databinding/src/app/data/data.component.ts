a31e8e3e181c6b559d4b2732fef60fb531ba7279
Krushna Rohitkumar Patel <Krushna.Patel@igtsolutions.com>
2025-04-04 18:24:09 +0530

    title change

--- Practice8-databinding/databinding/src/app/data/data.component.ts
+++ Practice8-databinding/databinding/src/app/data/data.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./data.component.css'],
 })
 export class DataComponent {
-  title = 'Data Binding';
+  title = 'Data Binding Page';
   imageUrl =
     'https://th.bing.com/th/id/R.4497d0dcdcda12265350cbac6a399b93?rik=R4uXNrSYdLYsRg&riu=http%3a%2f%2fwww.spholidays.com%2fimages%2fdelhi-travel-packages.jpg&ehk=6b0lX2ySW00z3Oyz7BhLjJxSL7UG6ckxI3%2btZvDLB9I%3d&risl=&pid=ImgRaw&r=0';
   count = 0;
